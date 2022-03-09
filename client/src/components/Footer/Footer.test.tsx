fb696a59186a38735d9a4e72af4c1c93e6f813d7
Carlos Daniel <ccdd.dev@gmail.com>
2022-03-09 00:08:51 -0600

    ref: do not duplicate getByText function

--- client/src/components/Footer/Footer.test.tsx
+++ client/src/components/Footer/Footer.test.tsx
@@ -1,43 +1,35 @@
 import "@testing-library/jest-dom";
 
 import { render } from "@project/tests/utils";
-import {
-  Matcher,
-  SelectorMatcherOptions,
-} from "@testing-library/react";
+import { getByText } from "@testing-library/dom";
 
 import Footer from "./Footer";
 
 describe("renders content", () => {
   let _container: HTMLElement;
-  let _getByText: (
-    id: Matcher,
-    options?: SelectorMatcherOptions | undefined
-  ) => HTMLElement;
 
   beforeEach(() => {
-    const { container, getByText } = render(<Footer />);
+    const { container } = render(<Footer />);
     _container = container;
-    _getByText = getByText;
   });
 
   test("menu is included", () => {
     let menuItems = _container.getElementsByClassName("footer-menu-item");
     expect(menuItems.length).toEqual(4);
 
-    expect(_getByText("Market")).toBeInTheDocument();
-    expect(_getByText("Exchange")).toBeInTheDocument();
-    expect(_getByText("Tutorials")).toBeInTheDocument();
-    expect(_getByText("Wallets")).toBeInTheDocument();
+    expect(getByText(_container, "Market")).toBeInTheDocument();
+    expect(getByText(_container, "Exchange")).toBeInTheDocument();
+    expect(getByText(_container, "Tutorials")).toBeInTheDocument();
+    expect(getByText(_container, "Wallets")).toBeInTheDocument();
   });
 
   test("contact info is included", () => {
-    expect(_getByText("Come join us")).toBeInTheDocument();
-    expect(_getByText("info@krypt.com")).toBeInTheDocument();
+    expect(getByText(_container, "Come join us")).toBeInTheDocument();
+    expect(getByText(_container, "info@krypt.com")).toBeInTheDocument();
   });
 
   test("legal rights are included", () => {
-    expect(_getByText("@krypt")).toBeInTheDocument();
-    expect(_getByText("All rights reserved")).toBeInTheDocument();
+    expect(getByText(_container, "@krypt")).toBeInTheDocument();
+    expect(getByText(_container, "All rights reserved")).toBeInTheDocument();
   });
 });
