71c51818c869ebc81db50d2b38c5fc7bedef5fd5
ccddan <ccddan>
2022-03-13 23:42:45 -0600

    fix: tsconfig alias resolutions (vite)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
@@ -5,4 +6,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@project/config': path.resolve(__dirname, '.', 'src', 'config', 'index.ts'),
+      '@project/config/*': path.resolve(__dirname, '.', 'src', 'config', '*'),
+    },
+  }
 });
